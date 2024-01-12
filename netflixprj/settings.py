65c239df9922020ceeabee470a45e677e1802350
Badar Islam <badarislam94@gmail.com>
2024-01-12 15:30:37 -0400

    allowing hosts

--- netflixprj/settings.py
+++ netflixprj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)#*#ia)5-81@vc%+)nfr!d#tfc#7k6e(1@$7st)cek)(9nwd0t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
